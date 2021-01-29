eb544e3b1b5ec6e0a11e0efee980c6f1a1fc2168
smiles365 <1052639169@qq.com>
2021-01-29 11:42:08 +0800

    new

--- UserProtect/src/service/UserService.java
+++ UserProtect/src/service/UserService.java
@@ -4,10 +4,7 @@ public interface UserService {
  void fun();
  void fun2();
  void fun3();
-//<<<<<<< HEAD
  void funslow();
-//=======
  void funquick();
-//>>>>>>> branch 'master' of https://github.com/smiles365/gz2071a.git
  
 }
